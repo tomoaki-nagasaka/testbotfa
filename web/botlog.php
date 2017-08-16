891965b68462af165dbf437823df1fb15b1a51aa
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-08-16 13:52:40 +0900

    更新

--- web/botlog.php
+++ web/botlog.php
@@ -10,10 +10,31 @@ $db_user =  getenv('DB_USER');
 $conn = "host=".$db_host." dbname=".$db_name." user=".$db_user." password=".$db_pass;
 $link = pg_connect($conn);
 
+
+
 if ($link) {
-	$result = pg_query("SELECT contents FROM botlog ORDER BY no DESC");
+	$result = pg_query("SELECT * FROM botlog");
+	echo "<table border=1 style=border-collapse:collapse;>";
+	echo "<tr><th>No</th><th>日時</th><th>ユーザーID</th><th>質問内容</th><th>回答内容</th></tr>";
 	while ($row = pg_fetch_row($result)) {
-		echo "<br>";
+		echo "<tr>";
+		echo "<td>";
 		echo $row[0];
+		echo "</td>";
+		echo "<td>";
+		echo $row[1];
+		echo "</td>";
+		echo "<td>";
+		echo $row[2];
+		echo "</td>";
+		echo "<td>";
+		echo $row[3];
+		echo "</td>";
+		echo "<td>";
+		echo $row[4];
+		echo "</td>";
+		echo "</tr>";
 	}
+	echo "</table>";
+	echo "<br>";
 }
