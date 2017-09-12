de7e4f01e7b240294b2c6714a06313cf36ff8514
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-09-12 11:30:32 +0900

    更新

--- web/callback.php
+++ web/callback.php
@@ -182,8 +182,9 @@ if($type != "text"){
 		$result = curl_exec ( $ch );
 
 		//$data= $result;
-		$data = array("images_file" => $result, "classifier_ids" => "garbage_2067461823");
+		//$data = array("images_file" => $result, "classifier_ids" => "garbage_2067461823");
 		//$data = array("images_file" => $result);
+		$data = array($result);
 		//$data = imagecreatefromstring($result);
 		if($data == false){
 			error_log("イメージ変換エラー");
