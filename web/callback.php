b096f2573acd5c016cb144b584e318ffd2905e07
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-09-13 11:40:55 +0900

    更新

--- web/callback.php
+++ web/callback.php
@@ -181,7 +181,8 @@ if($type != "text"){
 		) );
 		$result = curl_exec ( $ch );
 
-		$data= $result;
+		//$data= $result;
+		$data= array($result);
 		//$data = array('images_file' => $result, 'classifier_ids' => 'garbage_2067461823');
 		//$data = $result."&classifier_ids='garbage_2067461823'&threshold=0.0";
 		//$data = array($result,  "classifier_ids" => "garbage_2067461823", "threshold" => 0.0);
