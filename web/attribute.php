f5bc14982ff662a12dcf37a056a32ffc50b913e8
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-10-11 14:40:15 +0900

    更新

--- web/attribute.php
+++ web/attribute.php
@@ -146,7 +146,7 @@
 <option value="北地区">北地区</option>
 </select>
 <br>
-<input type="button" onclick="clear()" value="クリア" />
+<input type="button" onclick="clearform()" value="クリア" />
 <input type="button" onclick="update()" value="更新" />
 <input type="button" onclick="del()" value="削除" />
 
@@ -160,7 +160,7 @@ $user = $_GET['user'];
 <script>
 
 //クリア
-function clear(){
+function clearform(){
 	alert("押された");
 	/*
 	document.getElementsByName('age').selectedIndex = 0;
