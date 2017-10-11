18dab217639675b81fe4d83a44a4db14d7e12fdc
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-10-11 16:29:17 +0900

    更新

--- web/userinfoup.php
+++ web/userinfoup.php
@@ -17,13 +17,25 @@ $sex= $_POST['sex'];
 $region= $_POST['region'];
 
 
-error_log("user:".$user." age:".$age." sex:".$sex." region:".$region);
-/*
-if ($link) {
-	$result = pg_query("DELETE FROM botlog WHERE NO = ".$delno);
+//error_log("user:".$user." age:".$age." sex:".$sex." region:".$region);
 
+if ($link) {
+	$result = pg_query("SELECT * FROM userid WHERE userid = '{$user}'");
+	if (pg_num_rows($result) == 0) {
+		$sql = "INSERT INTO userinfo (userid, sex, age, region) VALUES ('{$user}','{$sex}','{$age}','{$region}')";
+		$result_flag = pg_query($sql);
+		if (!$result_flag) {
+			error_log("インサートに失敗しました。".pg_last_error());
+		}
+	}else{
+		$sql = "UPDATE userinfo SET sex = '{$sex}', age = '{$age}' , region = '{$region}' WHERE userid = '{$user}'";
+		$result_flag = pg_query($sql);
+		if (!$result_flag) {
+			error_log("アップデートに失敗しました。".pg_last_error());
+		}
+	}
 }
-*/
+
 
 ?>
 
