252dc1bcb84c56d3651200f543881a846af7afc7
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-10-11 14:41:18 +0900

    更新

--- web/attribute.php
+++ web/attribute.php
@@ -161,13 +161,9 @@ $user = $_GET['user'];
 
 //クリア
 function clearform(){
-	alert("押された");
-	/*
 	document.getElementsByName('age').selectedIndex = 0;
 	document.getElementsByName('sex').selectedIndex = 0;
 	document.getElementsByName('region').selectedIndex = 0;
-	alert(document.getElementsByName('age').selectedValue);
-	*/
 }
 
 //更新
