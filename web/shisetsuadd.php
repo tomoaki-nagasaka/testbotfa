8412bc1ba3a12e639a7025edb3271e49ac33080e
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-10-24 15:51:46 +0900

    更新

--- web/shisetsuadd.php
+++ web/shisetsuadd.php
@@ -7,150 +7,37 @@
 </head>
 <body>
 <p>施設名称</p>
-<input id="meisho" maxlength="40" placeholder="施設名称を入力してください">
+<input id="meisho" maxlength="40" placeholder="施設名称を入力してください" style="width: 500px;">
 <p>住所</p>
-<input id="jusho" maxlength="128" placeholder="施設の住所を入力してください">
-
-<p>年齢を選択してください。</p>
-<select id="age">
-<option value="999" selected>年齢</option>
-<option value="0">0歳</option>
-<option value="1">1歳</option>
-<option value="2">2歳</option>
-<option value="3">3歳</option>
-<option value="4">4歳</option>
-<option value="5">5歳</option>
-<option value="6">6歳</option>
-<option value="7">7歳</option>
-<option value="8">8歳</option>
-<option value="9">9歳</option>
-<option value="10">10歳</option>
-<option value="11">11歳</option>
-<option value="12">12歳</option>
-<option value="13">13歳</option>
-<option value="14">14歳</option>
-<option value="15">15歳</option>
-<option value="16">16歳</option>
-<option value="17">17歳</option>
-<option value="18">18歳</option>
-<option value="19">19歳</option>
-<option value="20">20歳</option>
-<option value="21">21歳</option>
-<option value="22">22歳</option>
-<option value="23">23歳</option>
-<option value="24">24歳</option>
-<option value="25">25歳</option>
-<option value="26">26歳</option>
-<option value="27">27歳</option>
-<option value="28">28歳</option>
-<option value="29">29歳</option>
-<option value="30">30歳</option>
-<option value="31">31歳</option>
-<option value="32">32歳</option>
-<option value="33">33歳</option>
-<option value="34">34歳</option>
-<option value="35">35歳</option>
-<option value="36">36歳</option>
-<option value="37">37歳</option>
-<option value="38">38歳</option>
-<option value="39">39歳</option>
-<option value="40">40歳</option>
-<option value="41">41歳</option>
-<option value="42">42歳</option>
-<option value="43">43歳</option>
-<option value="44">44歳</option>
-<option value="45">45歳</option>
-<option value="46">46歳</option>
-<option value="47">47歳</option>
-<option value="48">48歳</option>
-<option value="49">49歳</option>
-<option value="50">50歳</option>
-<option value="51">51歳</option>
-<option value="52">52歳</option>
-<option value="53">53歳</option>
-<option value="54">54歳</option>
-<option value="55">55歳</option>
-<option value="56">56歳</option>
-<option value="57">57歳</option>
-<option value="58">58歳</option>
-<option value="59">59歳</option>
-<option value="60">60歳</option>
-<option value="61">61歳</option>
-<option value="62">62歳</option>
-<option value="63">63歳</option>
-<option value="64">64歳</option>
-<option value="65">65歳</option>
-<option value="66">66歳</option>
-<option value="67">67歳</option>
-<option value="68">68歳</option>
-<option value="69">69歳</option>
-<option value="70">70歳</option>
-<option value="71">71歳</option>
-<option value="72">72歳</option>
-<option value="73">73歳</option>
-<option value="74">74歳</option>
-<option value="75">75歳</option>
-<option value="76">76歳</option>
-<option value="77">77歳</option>
-<option value="78">78歳</option>
-<option value="79">79歳</option>
-<option value="80">80歳</option>
-<option value="81">81歳</option>
-<option value="82">82歳</option>
-<option value="83">83歳</option>
-<option value="84">84歳</option>
-<option value="85">85歳</option>
-<option value="86">86歳</option>
-<option value="87">87歳</option>
-<option value="88">88歳</option>
-<option value="89">89歳</option>
-<option value="90">90歳</option>
-<option value="91">91歳</option>
-<option value="92">92歳</option>
-<option value="93">93歳</option>
-<option value="94">94歳</option>
-<option value="95">95歳</option>
-<option value="96">96歳</option>
-<option value="97">97歳</option>
-<option value="98">98歳</option>
-<option value="99">99歳</option>
-<option value="100">100歳</option>
-<option value="101">101歳</option>
-<option value="102">102歳</option>
-<option value="103">103歳</option>
-<option value="104">104歳</option>
-<option value="105">105歳</option>
-<option value="106">106歳</option>
-<option value="107">107歳</option>
-<option value="108">108歳</option>
-<option value="109">109歳</option>
-<option value="110">110歳</option>
-<option value="111">111歳</option>
-<option value="112">112歳</option>
-<option value="113">113歳</option>
-<option value="114">114歳</option>
-<option value="115">115歳</option>
-<option value="116">116歳</option>
-<option value="117">117歳</option>
-<option value="118">118歳</option>
-<option value="119">119歳</option>
-<option value="120">120歳</option>
+<input id="jusho" maxlength="128" placeholder="施設の住所を入力してください"  style="width: 500px;">
+<p>電話番号</p>
+<input id="jusho" maxlength="14" placeholder="000-0000-0000"  style="width: 500px;">
+<p>ジャンル１</p>
+<select id="j1">
+<option value="グルメ" selected>グルメ</option>
+<option value="レジャー・観光・スポーツ">レジャー・観光・スポーツ</option>
+<option value="ホテル・旅館">ホテル・旅館</option>
+<option value="駅・バス・車・交通">駅・バス・車・交通</option>
+<option value="公共・病院・銀行・学校">公共・病院・銀行・学校</option>
+<option value="ショッピング">ショッピング</option>
+<option value="生活・不動産">生活・不動産</option>
+<option value="ビジネス・企業間取引">ビジネス・企業間取引</option>
 </select>
-<p>性別を選択してください。</p>
-<select id="sex">
+<p>ジャンル２</p>
+<select id="j2">
 <option value="0" selected>性別</option>
 <option value="1">男性</option>
 <option value="2">女性</option>
 </select>
-<p>お住いの地域を選択してください。</p>
-<select id="region">
-<option value="000" selected>地域</option>
-<option value="001">東地区</option>
-<option value="002">西地区</option>
-<option value="003">中地区</option>
-<option value="004">南地区</option>
-<option value="005">北地区</option>
+<p>緯度</p>
+<input id="lat" maxlength="14" placeholder="999.99999" style="width: 200px;">
+<p>経度</p>
+<input id="lng" maxlength="14" placeholder="999.99999" style="width: 200px;">
 </select>
+<p>画像URL ※必ずhttpsから始まるURLを指定してください</p>
+<input id="iurl" maxlength="300" placeholder="https://www.yyy.zzz.jpg" style="width: 500px;">
+<p>詳細URL</p>
+<input id="iurl" maxlength="300" placeholder="http://www.yyy.zzz.html" style="width: 500px;">
 <br>
 <input type="button" onclick="clearform()" value="クリア" />
 <input type="button" onclick="update()" value="更新" />
