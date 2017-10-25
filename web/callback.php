393f62a18bbec809338c110cf44b2607155a020c
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-10-25 16:07:54 +0900

    更新

--- web/callback.php
+++ web/callback.php
@@ -427,8 +427,8 @@ if($type == "location"){
 
 	//現在地から近い順に検索
 	if ($link) {
-		$result = pg_query("SELECT meisho,jusho,tel,imageurl,url,distance_spheroid(geom,ST_GeomFromText('POINT({$latitude} {$longitude})',4326),
-        'SPHEROID[\"GRS_1980\",6378137,298.257222101]') AS KYORI FROM shisetsu WHERE distance_spheroid(geom,ST_GeomFromText('POINT({$latitude} {$longitude})',4326),'SPHEROID[\"GRS_1980\",6378137,298.257222101]') < 1000) AS GISX
+		$result = pg_query("SELECT meisho,jusho,tel,imageurl,url,ST_Distance_Spheroid(geom,ST_GeomFromText('POINT({$latitude} {$longitude})',4326),
+        'SPHEROID[\"GRS_1980\",6378137,298.257222101]') AS KYORI FROM shisetsu WHERE ST_Distance_Spheroid(geom,ST_GeomFromText('POINT({$latitude} {$longitude})',4326),'SPHEROID[\"GRS_1980\",6378137,298.257222101]') < 1000) AS GISX
         ORDER BY GISX.KYORI ");
 		if (pg_num_rows($result) == 0) {
 			$resmess = "半径1Km以内にお探しの施設はありませんでした。";
