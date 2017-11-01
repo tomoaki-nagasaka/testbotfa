f6935a6f92217a3b21adcef63ab3e6ef506d6728
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-11-01 15:20:23 +0900

    更新

--- web/index.php
+++ web/index.php
@@ -3,6 +3,11 @@
 <head>
 <meta charset='utf-8'>
 <title>Menu</title>
+<link href="css/common.css" rel="stylesheet" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/jquery.bootgrid.css" rel="stylesheet" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
 </head>
 <body>
 <div id="header"></div>
@@ -13,8 +18,6 @@
 <input type="button" onclick="location.href='shisetsu.php'" value="施設情報" />
 <input type="button" onclick="location.href='genre.php'" value="施設ジャンル" />
 </body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
 <script>
 $(function(){
 	$("#header").load("header.html");
