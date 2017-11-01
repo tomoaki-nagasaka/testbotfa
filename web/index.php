7860f99ff61ab386f1d81e31d007f55351759172
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-11-01 17:12:53 +0900

    更新

--- web/index.php
+++ web/index.php
@@ -10,12 +10,12 @@
 </head>
 <body>
 <div id="header"></div>
-<input type="button" onclick="location.href='botlog.php'" value="ログ参照" />
-<input type="button" onclick="location.href='imagelog.php'" value="画像ログ参照" />
-<input type="button" onclick="location.href='rank.php'" value="ランキング" />
-<input type="button" onclick="location.href='attribute.php'" value="属性登録" />
-<input type="button" onclick="location.href='shisetsu.php'" value="施設情報" />
-<input type="button" onclick="location.href='genre.php'" value="施設ジャンル" />
+<input type="button" class="btn btn-primary" onclick="location.href='botlog.php'" value="ログ参照" />
+<input type="button" class="btn btn-primary" onclick="location.href='imagelog.php'" value="画像ログ参照" />
+<input type="button" class="btn btn-primary" onclick="location.href='rank.php'" value="ランキング" />
+<input type="button" class="btn btn-primary" onclick="location.href='attribute.php'" value="属性登録" />
+<input type="button" class="btn btn-primary" onclick="location.href='shisetsu.php'" value="施設情報" />
+<input type="button" class="btn btn-primary" onclick="location.href='genre.php'" value="施設ジャンル" />
 </body>
 <script>
 $(function(){
