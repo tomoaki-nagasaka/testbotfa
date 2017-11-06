3488e55317590179b674b49c868f70b9fe6767b7
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-11-06 21:21:15 +0900

    更新

---
+++ web/opinion.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="市政へのご意見">
+<title>市政へのご意見</title>
+<link href="css/common.css" rel="stylesheet" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/jquery.bootgrid.css" rel="stylesheet" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+<script src="js/jquery.bootgrid.js"></script>
+</head>
+<body>
+<div id="loader-bg">
+  <div id="loader">
+    <img src="img/loading.gif" width="80" height="80" alt="Now Loading..." />
+    <p>Now Loading...</p>
+  </div>
+</div>
+<div id="wrap" style="display:none">
+<div id="header"></div>
+<?php
+
+//環境変数の取得
+$db_host =  getenv('DB_HOST');
+$db_name =  getenv('DB_NAME');
+$db_pass =  getenv('DB_PASS');
+$db_user =  getenv('DB_USER');
+
+//DB接続
+$conn = "host=".$db_host." dbname=".$db_name." user=".$db_user." password=".$db_pass;
+$link = pg_connect($conn);
+
+
+
+if ($link) {
+	$result = pg_query("SELECT * FROM opinion");
+	echo "<table id='grid-basic' class='table table-condensed table-hover table-striped'>";
+	echo "<thead>";
+	echo "<tr><th data-column-id='no' data-type='numeric' data-identifier='true' data-width='3%'>No</th>
+               <th data-column-id='date' data-width='7%'>日時</th>
+               <th data-column-id='sex'  data-width='5%'>性別</th>
+               <th data-column-id='age'  data-width='5%'>年齢</th>
+               <th data-column-id='sadness'  data-width='10%'>悲しみ</th>
+               <th data-column-id='joy'  data-width='10%'>喜び</th>
+               <th data-column-id='fear'  data-width='10%'>恐れ</th>
+               <th data-column-id='disgust'  data-width='10%'>嫌悪</th>
+               <th data-column-id='anger'  data-width='10%'>怒り</th>
+               <th data-column-id='opinion'  data-width='30%'>ご意見</th>
+           </tr>";
+	echo "</thead>";
+	echo "<tbody>";
+	while ($row = pg_fetch_row($result)) {
+		echo "<tr>";
+		echo "<td>";
+		echo $row[0];
+		echo "</td>";
+		echo "<td>";
+		echo substr($row[1], 0,4)."/".substr($row[1], 4,2)."/".substr($row[1], 6,2)." ".substr($row[1], 8,2).":".substr($row[1], 10,2);
+		echo "</td>";
+		echo "<td>";
+		echo $row[2];
+		echo "</td>";
+		echo "<td>";
+		echo $row[3];
+		echo "</td>";
+		echo "<td>";
+		echo $row[5];
+		echo "</td>";
+		echo "<td>";
+		echo $row[6];
+		echo "</td>";
+		echo "<td>";
+		echo $row[7];
+		echo "</td>";
+		echo "<td>";
+		echo $row[8];
+		echo "</td>";
+		echo "<td>";
+		echo $row[9];
+		echo "</td>";
+		echo "<td>";
+		echo $row[4];
+		echo "</td>";
+		echo "</tr>";
+	}
+	echo "</tbody>";
+	echo "</table>";
+	echo "<br>";
+}
+
+?>
+</div>
+<script>
+var rowIds = [];
+$(function() {
+	var h = $(window).height();
+	$('#wrap').css('display','none');
+	$('#loader-bg ,#loader').height(h).css('display','block');
+
+	$("#header").load("header.html");
+
+	$("#grid-basic").bootgrid({
+		selection: true,
+		multiSelect: true,
+		rowSelect: true,
+	    keepSelection: true,
+	}).on("selected.rs.jquery.bootgrid", function(e, rows)
+	{
+	    for (var i = 0; i < rows.length; i++)
+	    {
+	        rowIds.push(rows[i].id);
+	    }
+	    //alert("Select: " + rowIds.join(","));
+	}).on("deselected.rs.jquery.bootgrid", function(e, rows)
+	{
+	    for (var i = 0; i < rows.length; i++)
+	    {
+	    	rowIds.some(function(v, ii){
+	    	    if (v==rows[i].id) rowIds.splice(ii,1);
+	    	});
+	        //rowIds.push(rows[i].no);
+	    }
+	    //alert("Deselect: " + rowIds.join(","));
+	});
+});
+
+$(window).load(function () { //全ての読み込みが完了したら実行
+	  $('#loader-bg').delay(900).fadeOut(800);
+	  $('#loader').delay(600).fadeOut(300);
+	  $('#wrap').css('display', 'block');
+});
+
+
+</script>
+</body>
+</html>
+
